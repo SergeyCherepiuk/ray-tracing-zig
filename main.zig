54e6b31cb063497c4dd882138cc9efe0784a7e52
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2024-02-02 17:42:06 +0100

    Drawing circle shape

--- main.zig
+++ main.zig
@@ -1,9 +1,12 @@
 const std = @import("std");
-const image = @import("image.zig");
+// const image = @import("image.zig");
+const shapes = @import("shapes.zig");
 const ppm = @import("ppm.zig");
 
 pub fn main() !void {
-    const img = try image.Image.random(300, 300);
+    // const img = try image.Image.random(300, 300);
+    const img = try shapes.circle(300, 300, 50);
+
     const formated = try ppm.format(img);
     _ = try std.io.getStdOut().write(formated);
 }
