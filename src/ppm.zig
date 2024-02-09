e94bb853d681ef1f57cce28114267c08ceba3505
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2024-02-09 07:31:21 +0100

    Drastically reduced memory usage by using general-purpose allocator

--- src/ppm.zig
+++ src/ppm.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 const Image = @import("image.zig").Image;
 
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const allocator = gpa.allocator();
+
 /// Size of a single pixel record in bits. The size includes padding (if applicable)
 /// and a new line character for each record.
 const PixelRecordSize = 12;
@@ -11,8 +14,6 @@ pub const FormatError = error{
 };
 
 pub fn format(img: Image) FormatError![]const u8 {
-    const allocator = std.heap.page_allocator;
-
     const header: []const u8 = try std.fmt.allocPrint(
         allocator,
         "P3\n{d} {d}\n255\n",
@@ -21,10 +22,13 @@ pub fn format(img: Image) FormatError![]const u8 {
 
     const content_size = img.height * img.width * PixelRecordSize;
     const content: []u8 = try allocator.alloc(u8, content_size);
+    defer allocator.free(content);
 
+    const buf = try allocator.alloc(u8, PixelRecordSize);
+    defer allocator.free(buf);
     for (img.pixels, 0..) |pixel, i| {
         const record = try std.fmt.bufPrint(
-            try allocator.alloc(u8, PixelRecordSize),
+            buf,
             "{d:3} {d:3} {d:3}\n",
             .{ pixel.R, pixel.G, pixel.B },
         );
@@ -38,7 +42,6 @@ pub fn format(img: Image) FormatError![]const u8 {
 }
 
 fn concat(s1: []const u8, s2: []const u8) ![]const u8 {
-    const allocator = std.heap.page_allocator;
     const buf = try allocator.alloc(u8, s1.len + s2.len);
     std.mem.copy(u8, buf[0..s1.len], s1);
     std.mem.copy(u8, buf[s1.len..], s2);
